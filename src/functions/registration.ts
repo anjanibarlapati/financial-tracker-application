a740d32018908aaf2a472e6c0b9ed2b602b33cdb
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 17:52:19 +0530

    Handle username empty case in registration function

--- src/functions/registration.ts
+++ src/functions/registration.ts
@@ -3,7 +3,9 @@ import { users } from "../data/users";
 
 export function register(...credentials:string[]){
     const [username, password] = credentials;
-
+    if (!username) {
+        throw new Error('Username should be non-empty');
+    }
     const user = new User(username, password);
     users.push(user);
     return user;
