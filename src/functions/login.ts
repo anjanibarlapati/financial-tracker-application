6905ae8d9d8bf5bef02bd6b6e524832278c27f69
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 20:56:11 +0530

    Handle invalid username and incorrect password case in login function

--- src/functions/login.ts
+++ src/functions/login.ts
@@ -5,5 +5,10 @@ export function login(...credentials:string[]){
     const [username, password] = credentials;
 
     const user = users.find((user)=>user.username === username && user.password === password);
+
+    if(!user){
+        throw new Error('Username and password should be valid');
+    }
+    
     return user;
 }
