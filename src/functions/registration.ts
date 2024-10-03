614effaef69854a3ee3571583c27257f5c9b27cc
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 17:43:17 +0530

    Implement successfull registration functionality

--- src/functions/registration.ts
+++ src/functions/registration.ts
@@ -1,4 +1,10 @@
+import { User } from "../classes/users";
+import { users } from "../data/users";
 
 export function register(...credentials:string[]){
+    const [username, password] = credentials;
 
+    const user = new User(username, password);
+    users.push(user);
+    return user;
 }
