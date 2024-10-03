0c14ac3d8979c80adf604ff28778efefecd8ab27
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 20:58:47 +0530

    Handle missing username or password case in login function

--- src/functions/login.ts
+++ src/functions/login.ts
@@ -4,11 +4,15 @@ export function login(...credentials:string[]){
 
     const [username, password] = credentials;
 
+    if(credentials.length==1) {
+        throw new Error('Provide both username and password') ;
+    } 
+
     const user = users.find((user)=>user.username === username && user.password === password);
 
     if(!user){
         throw new Error('Username and password should be valid');
     }
-    
+
     return user;
 }
