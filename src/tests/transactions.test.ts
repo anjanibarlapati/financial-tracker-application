c1b94b96fb8376af3b37ee6d17238a129ca1070c
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 23:30:50 +0530

    Add tests for edge cases of transactions method

--- src/tests/transactions.test.ts
+++ src/tests/transactions.test.ts
@@ -1,13 +1,17 @@
+import { User } from "../classes/users";
 import { login } from "../functions/login";
 import { register } from "../functions/registration";
 import { ITransaction } from "../interfaces/transactions";
 
 
 describe("Transactions Functionality",()=>{
+    let user:User;
+    beforeAll(()=>{
+      user = register("anjani", "anjani123");
+    })
 
     test("Should credit given amount when all correct details are provided",()=>{
 
-        const user= register("anjani", "anjani123");      
         const txn:ITransaction = {id:user.transactions.length+1, type: "credit", amount:10000, category:"Salary", date:new Date()};
         user.transaction(txn);
 
@@ -16,8 +20,6 @@ describe("Transactions Functionality",()=>{
     });
 
     test("Should add credit amount to income if such category exists",()=>{
-
-        const user = login("anjani", "anjani123");
         
         const txn1:ITransaction = {id:user.transactions.length+1, type: "credit", amount:5000, category:"Others", date:new Date()};
         const txn2:ITransaction = {id:user.transactions.length+1, type: "credit", amount:5000, category:"Others", date:new Date()}
@@ -28,11 +30,25 @@ describe("Transactions Functionality",()=>{
     });
 
     test("Should debit given amount when all correct details are provided",()=>{
-        const user = login("anjani", "anjani123");
 
         const txn:ITransaction = {id:user.transactions.length+1, type: "debit", amount:1000, category:"Groceries", date:new Date()}
         user.transaction(txn);
         expect(user.availableBalance).toBe(19000);   
-    })
+    });
+
+    test("should throw an error when debit amount is more than the available balance", () => {
+
+        const txn:ITransaction = {id:user.transactions.length+1, type: "debit", amount:100000, category:"Salary", date:new Date()}
+        expect(() => user.transaction( txn)).toThrow("Insufficient balance");
+    });
+
+    test("should throw an error when transaction details invalid", () => {
+
+        const txn1:ITransaction = {id:user.transactions.length+1, type: "debit", amount:-100, category:"Salary", date:new Date()}
+        const txn2:ITransaction = {id:user.transactions.length+1, type: "debit", amount:100, category:"", date:new Date()}
+
+        expect(() => user.transaction( txn1)).toThrow("Transaction amount should greater than zero");
+        expect(() => user.transaction( txn2)).toThrow("Transaction category should be non-empty");
+    });
     
 })
