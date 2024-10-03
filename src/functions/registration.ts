d9da93e59af39c67c83e608f3d51104be7368494
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 19:50:34 +0530

    Handle case of existing user in registration function

--- src/functions/registration.ts
+++ src/functions/registration.ts
@@ -2,10 +2,18 @@ import { User } from "../classes/users";
 import { users } from "../data/users";
 
 export function register(...credentials:string[]){
+
     const [username, password] = credentials;
+
     if (!username || !password) {
         throw new Error('Username and password should be non-empty');
     }
+
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        throw new Error('Username is already exist');
+    };
+    
     const user = new User(username, password);
     users.push(user);
     return user;
