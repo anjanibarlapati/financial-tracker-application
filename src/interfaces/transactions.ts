dfe8e9dd7c90e9c9a17695ae5c9bb596157ef7f1
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-03 23:17:11 +0530

    Add test to adding credit amount to income if such category exists

--- src/interfaces/transactions.ts
+++ src/interfaces/transactions.ts
@@ -1,6 +1,5 @@
 export interface ITransaction  {
     id:number;
-    userId:number;
     type: 'debit' | 'credit';
     amount: number;
     category: string; 
--- src/tests/transactions.test.ts
+++ src/tests/transactions.test.ts
@@ -1,3 +1,4 @@
+import { login } from "../functions/login";
 import { register } from "../functions/registration";
 import { ITransaction } from "../interfaces/transactions";
 
@@ -7,11 +8,23 @@ describe("Transactions Functionality",()=>{
     test("Should credit given amount when all correct details are provided",()=>{
 
         const user= register("anjani", "anjani123");      
-        const txn:ITransaction = {id:user.transactions.length+1, userId: user.id, type: "credit", amount:10000, category:"Salary", date:new Date()};
+        const txn:ITransaction = {id:user.transactions.length+1, type: "credit", amount:10000, category:"Salary", date:new Date()};
         user.transaction(txn);
 
         expect(user.availableBalance).toBe(10000);  
         expect(user.totalIncome).toBe(10000);
+    });
+
+    test("Should add credit amount to income if such category exists",()=>{
+
+        const user = login("anjani", "anjani123");
+        
+        const txn1:ITransaction = {id:user.transactions.length+1, type: "credit", amount:5000, category:"Others", date:new Date()};
+        const txn2:ITransaction = {id:user.transactions.length+1, type: "credit", amount:5000, category:"Others", date:new Date()}
+        user.transaction(txn1);
+        user.transaction(txn2);
+        expect(user.availableBalance).toBe(20000);  
+        expect(user.totalIncome).toBe(20000);
     });
     
 })
