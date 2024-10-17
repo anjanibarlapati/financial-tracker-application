8658d6b3d49aad7cdba4b1da73f8dcea96fc533e
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-17 17:42:53 +0530

    Add test to verify app logo is rendered in register page

--- frontend/src/__tests__/Register.test.tsx
+++ frontend/src/__tests__/Register.test.tsx
@@ -1,14 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { Register } from '../components/Register';
 
-describe("Registartion Component", ()=>{
+describe("Registration Component", ()=>{
 
-    beforeAll(()=>{
+    beforeEach(()=>{
         render(<Register/>)
     })
     it("Should render fingrow title",()=>{
         const appTitle:HTMLElement = screen.getByText(/fingrow/i)
         expect(appTitle).toBeInTheDocument();
+    })
 
+    it("Should render application logo",()=>{
+        const appLogo: HTMLImageElement = screen.getByAltText("Logo");
+        expect(appLogo).toBeInTheDocument();
+        expect(appLogo).toHaveAttribute('src', '/assets/app-logo.png');
     })
 })
