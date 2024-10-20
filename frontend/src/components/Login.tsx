a55c06efbd90aed3c2707821348b1ba5ea5f310d
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 23:02:54 +0530

    Add login handler function

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import '../styles/Login.css';
+import { loginUser } from '../services/user';
 
 export function Login(): JSX.Element {
 
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+
+    async function loginHandler() {
+        try {
+            const user = await loginUser(username, password);
+        } catch (error: any) {
+            alert(`User login failed :(`)
+        }
+    }
 
     return (
         <div className="container">
@@ -16,7 +25,7 @@ export function Login(): JSX.Element {
             <div className='login-form'>
                 <input type="text" className="input" placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} />
                 <input type="password" className="input" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" className='login-button'>Login</button>
+                <button type="submit" className='login-button' onClick={async () => await loginHandler()}>Login</button>
             </div>
         </div>
     )
