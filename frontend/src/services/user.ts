8994a1a7808f1991b61a1949af719e71aa544559
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 18:56:39 +0530

    Add function to register user using axios

---
+++ frontend/src/services/user.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export async function registerUser(username:string, password:string) {
+    try {
+        const response = await axios.post('http://localhost:4321/register/user',{username:username, password:password});
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to register user');
+    }
+}
