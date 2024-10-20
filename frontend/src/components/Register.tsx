355062067e9bfb5300865347d7853adc07b83f72
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 18:45:56 +0530

    Add password input field  to Register form

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,13 +1,19 @@
 import '../styles/Register.css';
 
+
 export function Register(): JSX.Element {
+
     return (
         <div className="container">
-            <p className='app-title'>FinGrow</p>
-            <img src="/assets/app-logo.png" className='app-logo' alt='Logo'></img>
-            <form className='register-form'>
-                <input type="text" className="username" placeholder="Enter Username"/>
-            </form>
+            <div className='title-logo'>
+                <p className='app-title'>FinGrow</p>
+                <img src="/assets/app-logo.png" className='app-logo' alt='Logo'></img>
+            </div>
+            <p className='title'>Join FinGrow Now!!</p>
+            <div className='register-form'>
+                <input type="text" className="input" placeholder="Enter Username" />
+                <input type="password" className="input" placeholder="Enter Password"  />
+            </div>
         </div>
     )
 }
