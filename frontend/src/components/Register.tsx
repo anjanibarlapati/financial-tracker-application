8e777e82c9288633137f21e405799bebfc620389
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 18:57:55 +0530

    Add register button

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import '../styles/Register.css';
+import { registerUser } from '../services/user';
 
 
 export function Register(): JSX.Element {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
+    async  function registerHandler() {
+        try{
+           const user = await registerUser(username, password);
+        } catch(error:any){
+           alert(`Registering the user failed :(`)
+        }
+    }
 
     return (
         <div className="container">
@@ -17,6 +25,7 @@ export function Register(): JSX.Element {
             <div className='register-form'>
                 <input type="text" className="input" placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} />
                 <input type="password" className="input" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit" className='register' onClick={async () => await registerHandler()}>Register</button>
             </div>
         </div>
     )
