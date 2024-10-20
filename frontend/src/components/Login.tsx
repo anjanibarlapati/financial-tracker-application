0b5895fbd42f20e79cbef6beeb69db6e70e97cdb
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 22:20:02 +0530

    Add app title in Login page

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,14 @@
+import '../styles/Login.css';
+
+
+export function Login(): JSX.Element {
+
+    return (
+        <div className="container">
+            <div className='title-logo'>
+                <p className='app-title'>FinGrow</p>
+            </div>
+           
+        </div>
+    )
+}
