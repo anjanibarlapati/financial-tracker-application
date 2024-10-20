db66cfb12394450012c79b0ef6f3b5266cef576c
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 19:42:31 +0530

    Add test for user login api using axios

--- frontend/src/__tests__/userService.test.ts
+++ frontend/src/__tests__/userService.test.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { registerUser } from "../services/user";
+import { loginUser, registerUser } from "../services/user";
 
 jest.mock('axios');
 
-describe("User Routes", () => {
+describe("Register Route", () => {
     let user = {
         username: "abc",
         password: "abc",
@@ -31,5 +31,36 @@ describe("User Routes", () => {
         (axios.post as jest.Mock).mockRejectedValue(new Error("Failed to insert user"));
         
         await expect(registerUser("a","a")).rejects.toThrow('Failed to register user');
+    });
+});
+
+describe("Login Route", () => {
+    let user = {
+        username: "abc",
+        password: "abc",
+        transactions: [],
+        income: [],
+        availableBalance: 0,
+        totalIncome: 0,
+        budgets: [],
+        totalBudget: 0,
+        savingsGoals: [],
+    };
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    })
+
+    it("Should register a new user into the database", async () => {
+        (axios.post as jest.Mock).mockResolvedValue({data:user});
+        const response = await loginUser("a","a");
+        expect(axios.post).toHaveBeenCalledWith('http://localhost:4321/login/user', {username:"a", password:"a"});
+        expect(response).toEqual(user);
+    });
+
+    it("Should handle error when registering a new user", async () => {
+
+        (axios.post as jest.Mock).mockRejectedValue(new Error("Failed to find user"));
+        
+        await expect(loginUser("a","a")).rejects.toThrow('Failed to login user');
     });
 });
