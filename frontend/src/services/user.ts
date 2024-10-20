bd900278bc46646aab8d14dfab6853470e4da89f
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-20 19:43:18 +0530

    Add function for user login using axios

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -8,3 +8,12 @@ export async function registerUser(username:string, password:string) {
         throw new Error('Failed to register user');
     }
 }
+
+export async function loginUser(username:string, password:string) {
+    try {
+        const response = await axios.post('http://localhost:4321/login/user',{username:username, password:password});
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to login user');
+    }
+}
