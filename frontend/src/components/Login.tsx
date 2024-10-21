dcef6f8c377fc16b97356c1ee0dae97428840763
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 11:42:27 +0530

    Add register and homepage navigation in login page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import {  useState } from 'react';
 import '../styles/Login.css';
 import { loginUser } from '../services/user';
+import { useNavigate } from 'react-router-dom';
 
 export function Login(): JSX.Element {
 
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const navigate = useNavigate();
 
     async function loginHandler() {
         try {
             const user = await loginUser(username, password);
+            navigate("/homepage")
         } catch (error: any) {
             alert(`User login failed :(`)
         }
@@ -27,7 +30,7 @@ export function Login(): JSX.Element {
                 <input type="password" className="input" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit" className='login-button' onClick={async () => await loginHandler()}>Login</button>
             </div>
-            <p className='register-body'>Do not have an account?<span className='register-link'>Register</span> </p>
+            <p className='register-body'>Do not have an account?<span className='register-link' onClick={()=>navigate("/register")}>Register</span> </p>
         </div>
     )
 }
