6373311bac37a1c9d3fce1a78c792a2e272d15b0
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-22 03:15:40 +0530

    Add function to add transaction using axios

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ITransaction } from "../interfaces/transactions";
 
 export async function registerUser(username:string, password:string) {
     try {
@@ -15,5 +16,14 @@ export async function loginUser(username:string, password:string) {
         return response.data;
     } catch (error) {
         throw new Error('Failed to login user');
+    }
+}
+
+export async function addTransaction(transaction:ITransaction){
+    try {
+        const response = await axios.put(`http://localhost:4321/addTransaction/`, transaction);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error while inserting new transaction for the user');
     }
 }
