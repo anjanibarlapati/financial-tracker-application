45945a828542a1deeedc3396aaa348b95fcb6d05
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 23:39:57 +0530

    Add record expenses title

---
+++ frontend/src/components/RecordExpenses.tsx
@@ -0,0 +1,10 @@
+import '../styles/RecordExpenses.css';
+
+export function RecordExpenses(): JSX.Element {
+
+    return (
+        <div className="record-expenses-container">
+            <h1 className="record-expenses-title">Record Expenses</h1>
+        </div>
+    )
+}
