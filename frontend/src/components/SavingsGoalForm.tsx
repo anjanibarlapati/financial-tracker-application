a05174cd13664d071c494508e0c0a122ad349812
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-22 00:17:23 +0530

    Add savings goal form

---
+++ frontend/src/components/SavingsGoalForm.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import '../styles/SavingsGoalForm.css';
+
+export function SavingsGoalForm(): JSX.Element {
+
+    const [category, setCategory] = useState<string>('');
+    const [amount, setAmount] = useState<number>(0);
+    
+    return(
+        <div className="savings-goal-form-container">
+            <input type="text" className="savings-goal-input" placeholder="Category" onChange={(event)=>setCategory(event.target.value)}></input>
+            <input type="text" className="savings-goal-input" placeholder="Amount" onChange={(event)=>setAmount(Number(event.target.value))}></input>
+            <button className='savings-goal-button'>Create</button>
+        </div>
+    )
+}
