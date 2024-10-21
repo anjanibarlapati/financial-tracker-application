8d006d1d3eeea0c505a1460c0b213051c0574d5e
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 11:38:17 +0530

    Add routes to the main application

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import { Homepage } from './components/Homepage';
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/homepage',
+    element: <Homepage />
+  },
+
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={routes} />
   </React.StrictMode>
 );
 
