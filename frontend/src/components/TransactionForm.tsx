5084f9b29dc3233d6ece0419a0e11493fa3ff464
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-22 00:05:14 +0530

    Add transaction form component

---
+++ frontend/src/components/TransactionForm.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import '../styles/TransactionForm.css';
+
+export function TransactionForm(): JSX.Element {
+
+    const [transactionType, setTransactionType] = useState<string>('');
+    const [amount, setAmount] = useState<number>(0);
+    const [category, setCategory] = useState<string>('');
+    const [date, setDate] = useState<string>('');
+
+    return(
+        <div className="transaction-form-container">
+            <input type="text" className="transaction-input" placeholder="Transaction type" onChange={(event)=>setTransactionType(event.target.value)}></input>
+            <input type="text" className="transaction-input" placeholder="Amount" onChange={(event)=>setAmount(Number(event.target.value))}></input>
+            <input type="text" className="transaction-input" placeholder="Category" onChange={(event)=>setCategory(event.target.value)}></input>
+            <input type="text" className="transaction-input" placeholder="YYYY-MM-DD" onChange={(event)=>setDate(event.target.value)}></input>
+            <button className='transaction-button'>Add</button>
+        </div>
+    )
+}
