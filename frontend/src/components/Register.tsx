b19c14cd70f963be625fb183f20d00fa4c34437c
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 13:15:35 +0530

    Add user context and set user in register page

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import '../styles/Register.css';
 import { registerUser } from '../services/user';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../contexts/user';
 
 
 export function Register(): JSX.Element {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const {setCurrentUser} = useContext(UserContext);
     const navigate = useNavigate();
 
     async function registerHandler() {
         try {
             const user = await registerUser(username, password);
-            console.log("User registered");
+            setCurrentUser(user);
             navigate("/homepage");
         } catch (error: any) {
             alert(`Registering the user failed :(`)
