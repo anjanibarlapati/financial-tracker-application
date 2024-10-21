dbca1f4426bcf65a18bd9317da0d0c25bac08a4d
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 23:41:50 +0530

    Add adding transaction imageand text

--- frontend/src/components/RecordExpenses.tsx
+++ frontend/src/components/RecordExpenses.tsx
@@ -5,6 +5,14 @@ export function RecordExpenses(): JSX.Element {
     return (
         <div className="record-expenses-container">
             <h1 className="record-expenses-title">Record Expenses</h1>
+            <div className="record-expenses-icons">
+                <div className="icon-container">
+                    <div className="icon-body">
+                        <img src={'/assets/transaction.png'} alt="transaction" className="record-expenses-icon"></img>
+                        <h3 className="icon-title">Add Transaction</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
