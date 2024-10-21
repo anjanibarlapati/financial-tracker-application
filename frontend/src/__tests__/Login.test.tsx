c924df00618583a4ab7bcd81608629371d5307a5
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-21 13:15:46 +0530

    Add tests for user context in login page

--- frontend/src/__tests__/Login.test.tsx
+++ frontend/src/__tests__/Login.test.tsx
@@ -2,24 +2,62 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Login } from '../components/Login';
 import { loginUser } from '../services/user';
+import { UserContext } from '../contexts/user';
+import { ReactNode, useState } from 'react';
+import { IUser } from '../interfaces/user';
 
 jest.mock('../services/user', () => ({
     loginUser: jest.fn(),
 }));
 
 const mockNavigate = jest.fn();
+const mockSetCurrentUser = jest.fn();
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useNavigate: () => mockNavigate,
 }));
 
+const MockUserProvider = ({ children }: { children: ReactNode }) => {
+    const [currentUser, setCurrentUser] = useState<IUser>({
+        username: '',
+        password: '',
+        income: [],
+        totalIncome: 0,
+        transactions: [],
+        availableBalance: 0,
+        budgets: [],
+        totalBudget: 0,
+        savingsGoals: []
+    });
+
+    return (
+        <UserContext.Provider value={{ currentUser, setCurrentUser: mockSetCurrentUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
 describe("Login Component", () => {
+
+    let user = {
+        username: "abc",
+        password: "abc",
+        transactions: [],
+        income: [],
+        availableBalance: 0,
+        totalIncome: 0,
+        budgets: [],
+        totalBudget: 0,
+        savingsGoals: [],
+    };
 
     beforeEach(() => {
         render(
             <BrowserRouter>
-                <Login />
+                <MockUserProvider>
+                    <Login />
+                </MockUserProvider>
             </BrowserRouter>
         );
         jest.clearAllMocks();
@@ -111,28 +149,66 @@ describe("Login Component", () => {
 
     test('Should navigate to homepage after successful registration', async () => {
         (loginUser as jest.Mock).mockResolvedValueOnce({});
-    
+
         const usernameInput = screen.getByPlaceholderText(/enter username/i);
         const passwordInput = screen.getByPlaceholderText(/enter password/i);
         const loginButton = screen.getByText(/login/i);
-    
+
         fireEvent.change(usernameInput, { target: { value: 'anjani' } });
         fireEvent.change(passwordInput, { target: { value: 'anjani123' } });
-    
+
         fireEvent.click(loginButton);
-    
+
         await waitFor(() => {
             expect(mockNavigate).toHaveBeenCalledWith("/homepage");
         });
     });
-    
+
     it("should navigate to the register page when register link is clicked", () => {
         const registerLink = screen.getByText(/register/i);
-        waitFor(()=>{
+        waitFor(() => {
             fireEvent.click(registerLink);
 
         })
         expect(mockNavigate).toHaveBeenCalledWith("/register");
+    });
+
+    test('Should login user and set current user in context on successful login', async () => {
+        (loginUser as jest.Mock).mockResolvedValueOnce(user);
+
+        const usernameInput = screen.getByPlaceholderText(/enter username/i);
+        const passwordInput = screen.getByPlaceholderText(/enter password/i);
+        const loginButton = screen.getByText(/login/i);
+
+        fireEvent.change(usernameInput, { target: { value: 'anjani' } });
+        fireEvent.change(passwordInput, { target: { value: 'anjani123' } });
+        fireEvent.click(loginButton);
+
+        await waitFor(() => {
+            expect(mockSetCurrentUser).toHaveBeenCalledWith(user);
+        });
+    });
+
+
+    test('Should not set current user login failure', async () => {
+        const usernameInput = screen.getByPlaceholderText(/enter username/i);
+        const passwordInput = screen.getByPlaceholderText(/enter password/i);
+        const loginButton = screen.getByText(/login/i);
+    
+        (loginUser as jest.Mock).mockImplementation(() => {
+            throw new Error('Login failed');
+        });
+    
+        const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+
+    
+        fireEvent.change(usernameInput, { target: { value: 'anjani' } });
+        fireEvent.change(passwordInput, { target: { value: 'anjani123' } });
+        fireEvent.click(loginButton);
+
+        expect(mockSetCurrentUser).not.toHaveBeenCalled();
+        expect(alertSpy).toHaveBeenCalledWith('User login failed :(');
+        alertSpy.mockRestore();
     });
 
 })
