763c36aea0aebe1bbabab065a82dc5456f31b2e2
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-22 13:47:44 +0530

    Add income and and expenses report generation api using axios

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -2,25 +2,25 @@ import axios from "axios";
 import { ITransaction } from "../interfaces/transactions";
 import { ISavingsGoal } from "../interfaces/savingsGoals";
 
-export async function registerUser(username:string, password:string) {
+export async function registerUser(username: string, password: string) {
     try {
-        const response = await axios.post('http://localhost:4321/register/user',{username:username, password:password});
+        const response = await axios.post('http://localhost:4321/register/user', { username: username, password: password });
         return response.data;
     } catch (error) {
         throw new Error('Failed to register user');
     }
 }
 
-export async function loginUser(username:string, password:string) {
+export async function loginUser(username: string, password: string) {
     try {
-        const response = await axios.get('http://localhost:4321/login/user',{params: {username:username, password:password}});
+        const response = await axios.get('http://localhost:4321/login/user', { params: { username: username, password: password } });
         return response.data;
     } catch (error) {
         throw new Error('Failed to login user');
     }
 }
 
-export async function addTransaction(transaction:ITransaction){
+export async function addTransaction(transaction: ITransaction) {
     try {
         const response = await axios.put(`http://localhost:4321/addTransaction/`, transaction);
         return response.data;
@@ -29,20 +29,29 @@ export async function addTransaction(transaction:ITransaction){
     }
 }
 
-export async function addBudget(category:string, amount:number){
+export async function addBudget(category: string, amount: number) {
     try {
-        const response = await axios.put(`http://localhost:4321/addBudget/`, {category:category, amount:amount});
+        const response = await axios.put(`http://localhost:4321/addBudget/`, { category: category, amount: amount });
         return response.data;
     } catch (error) {
         throw new Error('Error while inserting new budget for the user');
     }
 }
 
-export async function addSavingsGoal(savingGoal:ISavingsGoal){
+export async function addSavingsGoal(savingGoal: ISavingsGoal) {
     try {
         const response = await axios.put(`http://localhost:4321/addSavingsGoal/`, savingGoal);
         return response.data;
     } catch (error) {
         throw new Error('Error while inserting new savings goal for the user');
+    }
+}
+
+export async function generateTotalIncomeAndExpenses(fromDate: Date, toDate: Date) {
+    try {
+        const response = await axios.get(`http://localhost:4321/generate/total-income-expenses`, { params: { fromDate: fromDate, toDate: toDate } });
+        return response.data;
+    } catch (error) {
+        throw new Error('Error while generating total income and expenses report of the user');
     }
 }
