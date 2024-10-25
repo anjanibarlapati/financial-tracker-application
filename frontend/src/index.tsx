0cee88641e51fc52b16a05d4bbde013073f7c145
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-10-25 11:03:14 +0530

    Remove commented lines

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
 import { Homepage } from './components/Homepage';
@@ -40,8 +39,3 @@ root.render(
       </UserProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
